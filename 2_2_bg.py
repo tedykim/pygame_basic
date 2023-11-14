015522bc843e654f6419e8355cf35c6fbc4150f9
tedykim <tedy0823@gmail.com>
2023-11-14 14:49:49 +0900

    pygame_basic,v1

---
+++ 2_2_bg.py
@@ -0,0 +1,25 @@
+import pygame
+pygame.init()
+# 화면 크기 설정
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width,screen_height))
+# 화면 타이틀 설정
+            # 게임 이름
+
+# 배경 이미지 불러오기
+
+# 이벤트 루프
+         # 게임이 진행중인가?
+             
+              # 어떤 이벤트가 발생하였는가?
+                         # 창이 닫히는 이벤트가 발생하였는가?
+                         # 게임이 진행중이 아님
+
+
+       # 배경 그리기
+
+     # 게임화면을 다시 그리기!
+
+# pygame 종료
+pygame.quit()
